32d0488ffa8f1fd0709d82bfc775a561756992e4
Dinir Imameev <dinir.imameev@gmail.com>
2022-03-17 17:03:15 -0400

    feat(task3): add source code

---
+++ task3/boolean_search.py
@@ -0,0 +1,93 @@
+import re
+
+
+def read_index(index_file):
+    f = open(index_file, "r")
+    lines = f.readlines()
+    index = dict()
+    for line in lines:
+        words = re.split('\s+', line)
+        index[words[0]] = []
+        for i in range(1, len(words) - 1):
+            index[words[0]].append(words[i])
+    for key, value in index.items():
+        index[key] = set(value)
+    return index
+
+
+def boolean_search(query, index):
+    and_operator = '&'
+    or_operator = '|'
+    not_operator = 'not'
+
+    all_pages = set()
+    for page in index.values():
+        all_pages = all_pages | page
+
+    def and_operation(set_a, set_b):
+        return set_a & set_b
+
+    def or_operation(set_a, set_b):
+        return set_a | set_b
+
+    def difference_set(set_a, set_b):
+        return set_a - set_b
+
+    words = re.split('\s+', query)
+    inversion = False
+
+    query_in_sets = []
+
+    prev_set = set()
+    for i in range(len(words)):
+        word = words[i]
+        if i == len(words) - 1:
+            if word not in index.keys():
+                prev_set = set()
+            elif inversion:
+                prev_set = difference_set(all_pages, index[word])
+            else:
+                prev_set = index[word]
+            query_in_sets.append((prev_set, None))
+        if word == and_operator or word == or_operator:
+            query_in_sets.append((prev_set, word))
+            continue
+
+        if word == not_operator:
+            inversion = True
+            continue
+        if word not in index.keys():
+            prev_set = set()
+        elif inversion:
+            prev_set = difference_set(all_pages, index[word])
+            inversion = False
+        else:
+            if word not in index.keys():
+                prev_set = set()
+            else:
+                prev_set = index[word]
+
+    def run_operations(query, operation_symbol, operation_func):
+        result = []
+        for i in range(len(query) - 1):
+            if query[i][1] == operation_symbol:
+                current_result = operation_func(query[i][0], query[i + 1][0])
+                query[i + 1] = (current_result, query[i + 1][1])
+            else:
+                result.append((query[i][0], query[i][1]))
+        result.append((query[len(query) - 1]))
+        return result
+
+    and_result = run_operations(query_in_sets, and_operator, and_operation)
+    or_result = run_operations(and_result, or_operator, or_operation)
+    if len(or_result) == 1:
+        print("Correct query")
+    return or_result[0][0]
+
+
+if __name__ == '__main__':
+    index = read_index("index.txt")
+    result = boolean_search("Яндекс", index)
+    print(len(result))
+    result = list(map(lambda x: x[4:6], result))
+    print(result)
---
+++ task3/tokens_lemmas.py
@@ -0,0 +1,120 @@
+import re
+import string
+import zipfile
+import ssl
+import nltk
+import pymorphy2
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+
+morph = pymorphy2.MorphAnalyzer()
+
+
+def tokenization(html):
+    soup = BeautifulSoup(html).get_text()
+    tokenization_condition = get_tokenization_condition()
+    result = list((filter(tokenization_condition, nltk.wordpunct_tokenize(soup))))
+    result = exclude_punctuation(result)
+    result = list(filter(exclude_trash(), result))
+    result = list(filter(exclude_numeral, result))
+    result = list(filter(exclude_not_permitted_symbols, result))
+    result = list(filter(exclude_glued_words, result))
+    return result
+
+
+def exclude_numeral(word):
+    regex = re.compile(r'^[0-9]+$')
+    if bool(regex.match(word.strip())):
+        return 1850 < int(word) < 2030
+    return True
+
+
+def exclude_not_permitted_symbols(word):
+    russian_words = re.compile(r'^[а-яА-Я]{2,}$')
+    english_words = re.compile(r'^[a-zA-Z]{2,}$')
+    numbers_words = re.compile(r'^[0-9]+$')
+    result = bool(russian_words.match(word)) or bool(english_words.match(word)) or bool(numbers_words.match(word))
+    return result
+
+
+def exclude_glued_words(word):
+    if word == word.upper():
+        return True
+    capitalize_word = word[0].upper() + word[1:]
+    split_result = re.findall(r'[А-ЯA-Z][^А-ЯA-Z]*', capitalize_word)
+    one_len_word_count = len(list(filter(lambda element: len(element) == 1, split_result)))
+    result = len(split_result) < 2 or one_len_word_count > 0
+    return result
+
+
+def exclude_trash():
+    trash = ['«', '»', '→', '·', '®', '▼', '–', '▸', 'x', 'X', ' ']
+    return lambda word: word not in trash
+
+
+def exclude_punctuation(values):
+    return [i for i in values if all(not j in string.punctuation for j in i)]
+
+
+def get_tokenization_condition():
+    stop_words = stopwords.words('russian')
+    stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', 'к', 'на', 'o'])
+    return lambda word: word not in stop_words
+
+
+def write_tokenization_result(result):
+    index_txt = open("tokens.txt", "w")
+    pattern = "%s\n"
+    for word in result:
+        index_txt.write(pattern % word)
+    index_txt.close()
+
+
+def get_lemma(word):
+    p = morph.parse(word)[0]
+    if p.normalized.is_known:
+        normal_form = p.normal_form
+    else:
+        normal_form = word.lower()
+    return normal_form
+
+
+def lemmatization(tokenization_result):
+    lemmatization_map = dict()
+    for word in tokenization_result:
+        normal_form = get_lemma(word)
+        if not normal_form in lemmatization_map:
+            lemmatization_map[normal_form] = []
+        lemmatization_map[normal_form].append(word)
+    return lemmatization_map
+
+
+def write_lemmatization_result(lemmatization_result):
+    file = open("lemmas.txt", "w")
+    for lemma, tokens in lemmatization_result.items():
+        file_string = lemma + " "
+        for token in tokens:
+            file_string += token + " "
+        file_string += "\n"
+        file.write(file_string)
+    file.close()
+
+
+def read_file(archive, file_name):
+    html = archive.open(file_name)
+    return html
+
+
+if __name__ == '__main__':
+    ssl._create_default_https_context = ssl._create_unverified_context
+    nltk.download('stopwords')
+    archive = zipfile.ZipFile('files.zip', 'r')
+    tokenization_result = set()
+    for file in archive.filelist:
+        current_html_file = read_file(archive, file.filename)
+        current_file_tokenization_result = set(tokenization(current_html_file))
+        tokenization_result = tokenization_result | current_file_tokenization_result
+        print("tokenization for", file.filename, "finished")
+    write_tokenization_result(tokenization_result)
+    lemmatization_result = lemmatization(tokenization_result)
+    write_lemmatization_result(lemmatization_result)
